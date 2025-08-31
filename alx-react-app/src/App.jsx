e90c911c6b30952d60d48ddefb219abc435fc1f8
Joy Shayan <kiamao1joy@gmail.com>
2025-08-31 20:30:04 +0300

    Add UserProfile component with props.

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,17 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+// src/App.jsx
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <h1>Welcome to My App</h1>
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
   );
 }
+
 export default App;
