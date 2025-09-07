9a410ae24e19ae099da827075df576503a218f92
Joy Shayan <kiamao1joy@gmail.com>
2025-09-07 15:57:59 +0300

    Add changes to MainContent

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,9 +3,7 @@ function MainContent() {
     <main style={{ padding: '20px', backgroundColor: '#eef2f7' }}>
       <h2 style={{ textAlign: 'center', color: 'darkslategray' }}>Welcome to My Profile</h2>
       <p style={{ marginTop: '10px', fontSize: '16px' }}>
-        I love to visit <span style={{ fontWeight: 'bold', color: 'darkblue' }}>New York</span>,{" "}
-        <span style={{ fontWeight: 'bold', color: 'darkred' }}>Paris</span>, and{" "}
-        <span style={{ fontWeight: 'bold', color: 'darkgreen' }}>Tokyo</span>.
+        I love to visit New York, Paris and Tokyo.
       </p>
     </main>
   );
