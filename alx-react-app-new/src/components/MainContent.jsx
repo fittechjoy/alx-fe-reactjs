f11a01eca6da8c88ddab7c8805d5733c950e25a2
fittechjoy <kiama01joy@gmail.com>
2025-09-14 23:41:44 +0300

    Update changes to MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -3,7 +3,7 @@ function MainContent() {
     <main style={{ padding: '20px', backgroundColor: '#eef2f7' }}>
       <h2 style={{ textAlign: 'center', color: 'darkslategray' }}>Welcome to My Profile</h2>
       <p style={{ marginTop: '10px', fontSize: '16px' }}>
-        I love to visit New York, Paris and Tokyo.
+        I love to visit New York, Paris, and Tokyo.
       </p>
     </main>
   );
