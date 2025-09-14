7b6618d1df73dc94c11583984e51409f119a8861
fittechjoy <kiama01joy@gmail.com>
2025-09-14 22:28:53 +0300

    Fix RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
@@ -12,7 +13,10 @@ const RecipeList = () => {
       <h2>Recipes</h2>
       <ul>
         {displayRecipes.map((recipe) => (
-          <li key={recipe.id}>{recipe.title}</li>
+          <li key={recipe.id}>
+            {/* ✅ Use Link to navigate to details */}
+            <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </li>
         ))}
       </ul>
     </div>
