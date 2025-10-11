ca7dbc9a6d39c586e053dfc37834a76c7d27197f
fittechjoy <kiama01joy@gmail.com>
2025-10-11 19:23:44 +0300

    queryClient

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import PostsComponent from "./components/PostsComponent";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+ const queryClient = new QueryClient();
+
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-2xl font-bold mb-4 text-center text-gray-800">
-        React Query Demo – Posts
-      </h1>
-      <PostsComponent />
-    </div>
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
   );
 }
 
